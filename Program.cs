e4022852a0033ca21b9165a45ba7668fddbd58f8
Nicholas <sousa.nicholas2003@gmail.com>
2024-01-08 21:22:00 -0300

    Mais um desafio concluido :)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone(numero : "123456", modelo: "iphone 14", imei: "123456789", memoria: "120");
+iphone.Ligar();
+iphone.InstalarAplicativo("spotify");
+
+Console.WriteLine("\n");
+
+Nokia nokia = new Nokia(numero : "654321", modelo: "nokia dobravel", imei: "987654321", memoria: "64");
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+
+
+
+
